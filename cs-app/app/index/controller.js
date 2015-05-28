70f045cde08c1e0e694ea94a1d6fb496a3cea913
Dmytro Dovhan <sinled@gmail.com>
2015-05-28 23:02:14 +0300

    added on sale filter

--- cs-app/app/index/controller.js
+++ cs-app/app/index/controller.js
@@ -1,7 +1,13 @@
 'use strict';
 import Ember from 'ember';
 
+const ON_SALE_COUNT = 3;
+
 export default Ember.ArrayController.extend({
   name: 'Dmytro',
-  productCount: Ember.computed.alias('length')
+  productCount: Ember.computed.alias('length'),
+
+  onSale: function() {
+    return this.filterBy('isOnSale').slice(0, ON_SALE_COUNT);
+  }.property('@each.isOnSale')
 });
--- cs-app/app/products/index/controller.js
+++ cs-app/app/products/index/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  someStrangeProperty: 'BANANA',
   sortProperties: ['title'],
   sortAscending: false
 });
