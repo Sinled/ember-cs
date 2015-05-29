30ca8d0cef2b326eca3b4da983fe6b57ceb5ffce
Dmytro Dovhan <sinled@gmail.com>
2015-05-29 09:56:45 +0300

    some refactor

--- cs-app/app/products/on-sale/route.js
+++ cs-app/app/products/on-sale/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    let products = this.store.find('products.product');
-    return products;
+    // receive data from parent route
+    return this.modelFor('products').filterBy('isOnSale');
   }
 });
