e1798644feee288eb1f5164c4ad9002eea447c9a
Dmytro Dovhan <d_dovgan@wargaming.net>
2015-05-29 13:46:08 +0300

    added product view

--- cs-app/app/components/product-details/component.js
+++ cs-app/app/components/product-details/component.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  reviewsCount: Ember.computed.alias('product.reviews.length'),
+  tagName: 'li',
+  classNames: ['row']
 });
--- cs-app/app/products/index/controller.js
+++ cs-app/app/products/index/controller.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   someStrangeProperty: 'BANANA',
   sortProperties: ['title'],
-  sortAscending: false
+  sortAscending: true
 });
