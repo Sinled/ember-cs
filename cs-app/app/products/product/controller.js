25c6eea733046ccd9f733b97d15234243104a042
Dmytro Dovhan <d_dovgan@wargaming.net>
2015-05-29 16:19:11 +0300

    added reviews

--- cs-app/app/products/product/controller.js
+++ cs-app/app/products/product/controller.js
@@ -1,5 +1,25 @@
+'use strict';
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isOnSale: Ember.computed.alias('model.isOnSale')
+  isOnSale: Ember.computed.alias('model.isOnSale'),
+  reviewText: '',
+
+  isSaveDisabled: function() {
+    return !this.get('reviewText').length;
+  }.property('reviewText'),
+
+  actions: {
+    createReview: function() {
+      if (this.get('reviewText')) {
+        this.store.createRecord('products.product.reviews.review', {
+          reviewedAt: new Date().getTime(),
+          text: this.get('reviewText'),
+          product: this.model
+        });
+
+        this.set('reviewText', '');
+      }
+    }
+  }
 });
