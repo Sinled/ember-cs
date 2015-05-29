45b9abaacc39718713ae56e826dc2b2ef3d17f31
Dmytro Dovhan <d_dovgan@wargaming.net>
2015-05-29 13:46:13 +0300

    added product view

---
+++ cs-app/tests/unit/products/product/view-test.js
@@ -0,0 +1,9 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('view:products/product', 'Unit | View | products/product');
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  var view = this.subject();
+  assert.ok(view);
+});
